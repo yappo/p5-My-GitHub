d42ccab83236e805f352c2afe377564019207f1a
yappo <yappo@shibuya.pl>
2009-12-19 21:34:23 +0900

    initial commit

---
+++ lib/My/GitHub.pm
@@ -0,0 +1,32 @@
+package My::GitHub;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+My::GitHub -
+
+=head1 SYNOPSIS
+
+  use My::GitHub;
+
+=head1 DESCRIPTION
+
+My::GitHub is
+
+=head1 AUTHOR
+
+Default Name E<lt>default {at} example.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
